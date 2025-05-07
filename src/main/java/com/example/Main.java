dc49b85c47b0359cd6311e459781fa224466ba3c
TheAtilax <117635766+TheAtilax@users.noreply.github.com>
2025-05-07 11:47:12 +0200

    Update Main.java

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -3,14 +3,15 @@ package com.example;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
-
     @Override
     public void onEnable() {
-        getLogger().info("Hello, World!");
+        getLogger().info("MyPlugin has been enabled!");
+        // Plugin startup logic here
     }
 
     @Override
     public void onDisable() {
-        getLogger().info("Goodbye, World!");
+        getLogger().info("MyPlugin has been disabled.");
+        // Plugin shutdown logic here
     }
 }
